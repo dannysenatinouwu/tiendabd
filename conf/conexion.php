be4325cb77af4353f8405058599a1a8b82f6e9f2
dannysenatinouwu <145168540+dannysenatinouwu@users.noreply.github.com>
2023-09-16 10:08:02 -0500

    Update conexion.php
    yafueya

--- conf/conexion.php
+++ conf/conexion.php
@@ -1,23 +1,7 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $clave = "";
-    $bd = "bd_tienda";
-    $conexion = mysqli_connect($host,$user,$clave,$bd);
-    if (mysqli_connect_errno()){
-        echo "No se pudo conectar a la base de datos";
-        exit();
-    }
-    mysqli_select_db($conexion,$bd) or die("No se encuentra la base de datos");
-    mysqli_set_charset($conexion,"utf8");
-    
-?>
-
-
-<?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:bdtienda.database.windows.net,1433; Database = dbtienda", "administrador", "@transformiceABC");
+    $conn = new PDO("sqlsrv:server = tcp:bdtienda.database.windows.net,1433; Database = dbtienda", "administrador", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
