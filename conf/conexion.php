39998b2602d146017dac1feac8e0a206c02b81cf
dannysenatinouwu <145168540+dannysenatinouwu@users.noreply.github.com>
2023-09-18 15:49:21 -0500

    Update conexion.php

--- conf/conexion.php
+++ conf/conexion.php
@@ -1,16 +1,15 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:bdtienda.database.windows.net,1433; Database = dbtienda", "administrador", "@transformiceABC");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "administrador", "pwd" => "@transformiceABC", "Database" => "dbtienda", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:bdtienda.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$connectionOptions = array(
+    "Database" => "dbtienda",
+    "Uid" => "administrador",
+    "PWD" => "@transformiceABC"
+);
+
+// Intentar establecer la conexión
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if (!$conn) {
+    die("La conexión a SQL Server falló: " . sqlsrv_errors());
+}
 ?>
